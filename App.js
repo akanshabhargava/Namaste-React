94c56ed3c1cadd578f01bd47a5c0b04b2d50a710
akanshabhargava <akansha.bhargava17@gmail.com>
2024-07-03 23:50:09 +0530

    changes in app

--- App.js
+++ App.js
@@ -61,19 +61,20 @@
     </div>
  </div>
  */
- const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    [React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I am a h1 tag"),
-      React.createElement("h2", {}, "I am a h2 tag"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I am a h1 tag"),
-        React.createElement("h2", {}, "I am a h2 tag"),
-      ]),]
-  );
- console.log(parent);
- const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(parent);
+//  const parent = React.createElement(
+//     "div",
+//     { id: "parent" },
+//     [React.createElement("div", { id: "child" }, [
+//       React.createElement("h1", {}, "I am a h1 tag"),
+//       React.createElement("h2", {}, "I am a h2 tag"),
+//     ]),
+//     React.createElement("div", { id: "child2" }, [
+//         React.createElement("h1", {}, "I am a h1 tag"),
+//         React.createElement("h2", {}, "I am a h2 tag"),
+//       ]),]
+//   );
+//  console.log(parent);
+//  const root = ReactDOM.createRoot(document.getElementById("root"));
+//  root.render(parent);
+let a =10;
 
